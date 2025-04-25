3ed52e46ee5b14f73d9b2bcdc8f25b7b37461b7a
soufiaChafiq20 <sofiachafiq@gmail.com>
2025-04-25 21:40:56 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-/* =========================== typing animation ============================= */ 
+// document.addEventListener("DOMContentLoaded", function() {
+/*======================== typing animation ============================= */ 
 var typed = new Typed(".typing",{ 
     strings:["","Computer Science Student","Future Engineer"],
     typeSpeed:100, 
@@ -9,7 +10,7 @@ var typed = new Typed(".typing",{
 /* =========================== Aside ============================= */ 
 const nav = document.querySelector(".nav"), 
       navList = nav.querySelectorAll("li"), 
-      totalNavList = navList.length;
+      totalNavList = navList.length,
       allSection=document.querySelectorAll(".section"),
       totalSection=allSection.length;
       for(let i=0; i<totalNavList; i++) 
@@ -46,7 +47,7 @@ const nav = document.querySelector(".nav"),
                 allSection[num].classList.add("back-section");
             }
             function showSection(element){
-                for(let i=0;i<totalSection;i++)
+                for(let i=0; i<totalSection; i++)
                 {
                     allSection[i].classList.remove("active");
                 }
@@ -59,33 +60,39 @@ const nav = document.querySelector(".nav"),
                     {
                         navList[i].querySelector("a").classList.remove("active");
                         const target = element.getAttribute("href").split("#")[1];
-                        if(target == navList[i].querySelector("a").getAttribute("href").split("#")[1])
+                        if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
                             {
-                                navList[i].querySelector("#").classList.add("active");
+                                navList[i].querySelector("a").classList.add("active");
                             }
                         }
                     }
                     // document.querySelector(".hire-me").addEventListener("click", function()
                     // {
                     //     const sectionIndex = this.getAttribute("data-section-index");
-                    //     console.log(SectionIndex);
+                    //     // console.log(SectionIndex);
                     //     showSection(this);
                     //     updateNav(this);
                     //     removeBackSection();
                     //     addBackSection(sectionIndex);
                     // });
-
-
-            const navTogglerBtn = document.querySelector(".nav-toggler");
-            const aside = document.querySelector(".aside");
-            const sections=document.querySelectorAll(".section");
-            navTogglerBtn.addEventListener("click", asideSectionTogglerBtn);
-            
-            function asideSectionTogglerBtn()
-                {
-                    aside.classList.toggle("open");
-                    navTogglerBtn.classList.toggle("open");
-                    sections.forEach(section => {
-                        section.classList.toggle("open");
-                    });
-                    }
+                    const navTogglerBtn = document.querySelector(".nav-toggler"),
+                          aside = document.querySelector(".aside");
+                    // const sections=document.querySelectorAll(".section");
+                           navTogglerBtn.addEventListener("click", () =>
+                            {
+                            asideSectionTogglerBtn();
+                            })
+                            function asideSectionTogglerBtn()
+                            {
+                                aside.classList.toggle("open");
+                                navTogglerBtn.classList.toggle("open");
+                    // sections.forEach(section => {
+                    //     section.classList.toggle("open");
+                    // });
+                    // }
+                                for(let i=0; i<totalSection; i++)
+                                {
+                                    allSection[i].classList.toggle("open");
+                                }
+                            }
+                // });
